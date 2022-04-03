029b18a6c80e0dc0df4006c0e84d540fb9a563cd
cong9c6s <93995859+buiminhcong@users.noreply.github.com>
2022-04-04 01:16:42 +0700

    ok

---
+++ app/src/main/java/com/bmcong2k/datepickerdialog/MainActivity.java
@@ -0,0 +1,66 @@
+package com.bmcong2k.datepickerdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText edt;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edt = findViewById(R.id.edt);
+        btn = findViewById(R.id.btnChon);
+
+        edt.setOnClickListener(this);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar c = Calendar.getInstance();
+                int y = c.get(Calendar.YEAR);
+                int m = c.get(Calendar.MONTH);
+                int d = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        edt.setText(year+"/"+month+"/"+dayOfMonth);
+                    }
+                },y,m,d);
+                dialog.show();
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v==edt){
+            Calendar c = Calendar.getInstance();
+            int y = c.get(Calendar.YEAR);
+            int m = c.get(Calendar.MONTH);
+            int d = c.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    edt.setText(year+"/"+month+"/"+dayOfMonth);
+                }
+            },y,m,d);
+            dialog.show();
+        }
+    }
+}
